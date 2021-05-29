ab745e74fb610d0fad75f8f85ecf99b63b8bab4c
lazzarolaura <lazzaro-laura@hotmail.fr>
2021-05-29 13:13:18 +0200

    install-vue

---
+++ strongandfit/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+
+<header>
+
+    <div>
+        <h6>Bonjour</h6>
+
+        <button>Déconnexion</button>
+    </div>
+
+</header>
+
+</template>
+
+<script>
+//import userService from '../services/userService.js';
+
+export default {
+/*      methods: {
+        disconnect: function() {
+            // descruction des données dans le session storage
+            userService.deleteCurrentUserData();
+            // nous "forçons" le header pour qu'il considère qu'il n'y a plus d'utilisateur connecté
+            this.currentUser = false;
+            // BONUS après déconnexion, redirige l'utilisateur vers la home
+            this.$router.push({
+                name: "home"
+            });
+        },
+    }, */
+}
+
+</script>
+
+<style scoped>
+</style>
---
+++ strongandfit/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+
+const router = new VueRouter(
+  {
+    
+  }
+)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
